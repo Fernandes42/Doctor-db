7b1da472086f60e50361fb46f06f8f512a9a7ce7
Vikram-T <8673341+Vikram-T@users.noreply.github.com>
2020-05-08 15:58:28 -0600

    Added very basic login screen

--- doctor_db/doctor_db/urls.py
+++ doctor_db/doctor_db/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
