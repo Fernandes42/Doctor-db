eeb70fff8d91837cbec9b7e64e4255d1e10bac6f
JuanNRamirez <nicoramirezjulio@gmail.com>
2020-05-09 13:48:49 -0500

    Merge branch 'master' of https://github.com/Fernandes42/Doctor-db into NicoWork

--- doctor_db/applications/forms.py
+++ doctor_db/applications/forms.py
@@ -5,8 +5,24 @@ SEX_CHOICE =(
     ("F", "Female"),
 )
 HOSPITALISED_CHOICE =(
-    ("M", "Male"),
-    ("F", "Female"),
+    ("Y", "Yes"),
+    ("N", "No"),
+)
+
+STATUS_CHOICE =(
+    ("A", "Active"),
+    ("D", "Deceased"),
+    ("R", "Recovered")
+)
+
+COVID_STRAIN =(
+    ("L", "L-Type"),
+    ("S", "S-Type"),
+)
+
+RESPIRATOR_CHOICE =(
+    ("Y", "Yes"),
+    ("N", "No"),
 )
 
 class Covid_form(forms.Form):
@@ -15,6 +31,9 @@ class Covid_form(forms.Form):
     race = forms.CharField(label='Race', max_length=10)
     country = forms.CharField(label='Country', max_length=20)
     hospital = forms.CharField(label='Hospital', max_length=30)
-    pre_exisiting = forms.CharField(label='Pre-exisiting health problems', max_length=10)
+    covid_strain = forms.ChoiceField(choices=COVID_STRAIN)
+    pre_exisiting = forms.CharField(label='Pre-existing health problems', max_length=10)
     hospitalised = forms.ChoiceField(choices = HOSPITALISED_CHOICE)
+    respirator_required = forms.ChoiceField(choices = RESPIRATOR_CHOICE)
     medicine_applied = forms.CharField(max_length=30)
+    current_status = forms.ChoiceField(choices= STATUS_CHOICE)
