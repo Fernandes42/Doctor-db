9df701c7b11113328c02ea5438a65de3fbf77855
Joshua Fernandes <josh.fernandes42@gmail.com>
2020-05-09 09:45:54 +0100

    added model

--- doctor_db/applications/models.py
+++ doctor_db/applications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Covid_tb(models.Model):
+    id = models.AutoField(primary_key=True)
+    age =  models.PositiveSmallIntegerField()
+    sex = models.CharField(max_length=1)
+    race = models.CharField(max_length=10)
+    country = models.CharField(max_length=20)
+    hospital = models.CharField(max_length=30)
+    pre_exisiting = models.CharField(max_length=50)
+    hospitalised = models.CharField(max_length=1)
+    medicine_applied = models.CharField(max_length=30)
